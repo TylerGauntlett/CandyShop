0d7590123eec9f1456f5bedf4b3ef5416abb6c19
Shyskel <tylergauntlett@hotmail.com>
2016-03-24 10:42:02 -0400

    Add comments and redesign solution
    Resigned program  using dynamic programming technique. Added comments
    detailing what is happening. Program now works properly.

--- CandyMain.java
+++ CandyMain.java
@@ -4,63 +4,86 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Scanner;
 
-import javax.swing.plaf.InputMapUIResource;
-
 public class CandyMain {
-	
-	public static final int WIDTH = 2;
 
 	public static void load() throws URISyntaxException, IOException {
 
 		URL openKeyFile = CandyMain.class.getResource("input.txt");
 		Scanner sc = new Scanner(new File(openKeyFile.toURI()));
 		
-		// Input for debugging.
+		// Used to output for debugging.
 		int inputCase = 1;
-		
+
 		int numOfCandy = sc.nextInt();
-		double totalCash = sc.nextDouble();
-		
-		
-		while(numOfCandy != 0 && totalCash != 0)
-		{
-			System.out.println("Input put case " + inputCase + ":");
-			// First index contains the number
-			double[][] inputMatrix = new double[numOfCandy][WIDTH];
+		double totalMoney = sc.nextDouble();
+
+		// 0 0.00 is the terminating condition for accepting input.
+		while (numOfCandy != 0 && totalMoney != 0) {
 			
+			// Print statement for debugging.
+			System.out.println("Input put case " + inputCase + " with $" + totalMoney + ":");
+			
+			// Initialize both input arrays.
+			int[] caloriesArray = new int[numOfCandy];
+			double[] moneyArray = new double[numOfCandy];
+
+			// Loads both arrays with value from an input file.
 			for(int i = 0; i < numOfCandy; i++)
 			{
-				for(int j = 0; j < WIDTH; j++)
-				{
-					inputMatrix[i][j] = sc.nextDouble();
-				}
-					
+				caloriesArray[i] = sc.nextInt();
+				moneyArray[i] = sc.nextDouble();
 			}
 			
+			// Prints both arrays for debugging purposes.
 			for(int i = 0; i < numOfCandy; i++)
 			{
-				for(int j = 0; j < WIDTH; j++)
-				{
-					System.out.print(inputMatrix[i][j] + " ");
-				}
-				System.out.println();
-					
+				System.out.print(caloriesArray[i] + " ");
+				System.out.println(moneyArray[i]);
 			}
 			
+			// Initialize a used array.
+			boolean[] used = new boolean[numOfCandy];
+			
+
+			// Call the function that returns the most calories possible.
+			int mostCalories = findMostCalories(caloriesArray, moneyArray, totalMoney, numOfCandy-1, used);
+
+			System.out.println(mostCalories);
+
+			// Reset used array before next looping.
+			for(int i = 0; i < numOfCandy; i++)
+				used[i] = false;
+			
+			// Increase case count.
 			inputCase++;
-			
-			// Take in the next line
+
+			// Take in the next line and compare at while loop.
 			numOfCandy = sc.nextInt();
-			totalCash = sc.nextDouble();
-			
-			if(numOfCandy == 0 && totalCash == 0)
-				break;
+			totalMoney = sc.nextDouble();
+
 		}
-		
-		
-		
+
+		sc.close();
 	}
-	
+
+	// This would would the problem and return the smallest amount of calories.
+	public static int findMostCalories(int[] calories, double[] money, double cash, int index, boolean[] used) {
+
+		if(index < 0)
+			return 0;
+		
+		if(money[index] > cash)
+			return findMostCalories(calories, money, cash, index-1, used);
+		
+		double one = findMostCalories(calories, money, cash, index-1, used);
+		double two = findMostCalories(calories, money, cash - money[index], index, used) + calories[index];
+		
+		if(one < two)
+			used[index] = true;
+		
+		return (int) (Math.max(one, two));
+	}
+
 	public static void main(String[] args) {
 
 		try {
